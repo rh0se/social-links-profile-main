dd5e893f544862caf831fefecf46467b60ff68a2
rh0se <momirhoda@gmail.com>
2024-08-02 14:35:05 +0100

    worked on the styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme  = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans]
+      },
+      spacing: {
+        '128': '30rem',
+      },
+      colors: {
+        green: 'hsl(75, 94%, 57%)',
+        white: 'hsl(0, 0%, 100%)',
+        grey700: 'hsl(0, 0%, 20%)',
+        grey800: 'hsl(0, 0%, 12%)',
+        grey900: 'hsl(0, 0%, 8%)',
+      }
+    },
   },
   plugins: [],
 }
